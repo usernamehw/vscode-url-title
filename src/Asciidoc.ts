62e16a6243e0256c53fbf76dd2ff9a65cfb4fcb8
Alexander <usernamehw@gmail.com>
2024-08-18 23:13:41 +0300

    chore: update dependencies

--- src/Asciidoc.ts
+++ src/Asciidoc.ts
@@ -1,16 +1,16 @@
 import type { TextDocument } from 'vscode';
 
-export const Asciidoc = {
+export const AsciiDoc = {
 	isAsciidocFile(document: TextDocument): boolean {
 		return document.uri.fsPath.endsWith('.adoc') ||
-		document.uri.fsPath.endsWith('.asciidoc');
+			document.uri.fsPath.endsWith('.asciidoc');
 	},
 	isAsciidocUrlHasTitle(urlString: string): boolean {
 		return urlString.includes('[') &&
-		urlString.endsWith(']');
+			urlString.endsWith(']');
 	},
 	getAsciidocUrl(urlString: string): string {
-		if (Asciidoc.isAsciidocUrlHasTitle(urlString)) {
+		if (AsciiDoc.isAsciidocUrlHasTitle(urlString)) {
 			return urlString.slice(0, urlString.lastIndexOf('['));
 		} else {
 			return urlString;
